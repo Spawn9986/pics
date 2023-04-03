9ba84661d60608035d0102ff65c1c44abbe6911a
Spawn9986 <scouch9986@gmail.com>
2023-04-02 20:53:38 -0500

    added documentation for later reference

--- src/App.js
+++ src/App.js
@@ -14,8 +14,9 @@ function App() {
         the callback (handleSubmit). Within this callback function we can pass in 
         any data (as an argument ...in this case "term" correlating to whatever 
         the user enters into the search bar as a search request. The data will
-        eventually show back up in the parent (App.js) */}
-        <SearchBar onSubmit={handleSubmit}/>
+        eventually show back up in the parent (App.js). We defined SearchBar above
+        and now we will render it through < /> */}
+        <SearchBar onSubmit={handleSubmit}/> 
     </div>
 }
 
