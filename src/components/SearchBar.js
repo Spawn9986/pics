35352c292626f4dd88fec6664ce09b7bae9dc518
Spawn9986 <scouch9986@gmail.com>
2023-04-03 18:59:22 -0500

    set the search bar input to the state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
-function SearchBar() {
+import { useState } from 'react';
+
+function SearchBar({ onSubmit }) {
+    const [term, setTerm] = useState('');
+
     const handleFormSubmit = (event) => {
         event.preventDefault();
-        console.log('I need to tell the parent about some data');
+        
+        onSubmit('cars');
+    };
+
+    const handleChange = (event) => {
+        setTerm(event.target.value);
     };
     
     return (
     <div>
         <form onSubmit={handleFormSubmit}>
-            <input />
+            <input value={term} onChange={handleChange}/>
         </form>
     </div>
     );
