38d5a0079e463d10ff4b55fb2b637d2c3d0480ab
Spawn9986 <scouch9986@gmail.com>
2023-04-03 17:39:14 -0500

    added a form to detect a user typing text into a search bar & pressing enter and passed in an event and prevented the form default behavior

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,16 @@
-function SearchBar({ onSubmit }) {
-    const handleClick = () => {
-        onSubmit('cars');
+function SearchBar() {
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log('I need to tell the parent about some data');
     };
-
-    return <div>
-        <input />
-        <button onClick={handleClick}>Click me</button>
-    </div>;
+    
+    return (
+    <div>
+        <form onSubmit={handleFormSubmit}>
+            <input />
+        </form>
+    </div>
+    );
 }
 
 export default SearchBar;
